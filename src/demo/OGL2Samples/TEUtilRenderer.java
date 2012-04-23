0b475e47d783042878881bb3a27a84b435016ab4
Matt Galaviz <matthew.galaviz@gmail.com>
2012-04-22 23:25:05 -0700

    cleaning

--- src/demo/OGL2Samples/TEUtilRenderer.java
+++ src/demo/OGL2Samples/TEUtilRenderer.java
@@ -5,16 +5,12 @@ import javax.microedition.khronos.opengles.GL10;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
-import android.util.Log;
 
 public class TEUtilRenderer implements GLSurfaceView.Renderer {
 
     public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
         GLES20.glEnable(GL10.GL_BLEND);
 		GLES20.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
-		
-		//TEManagerGraphics.createPrograms();
-        //mGame.start();
     }
 
     public void onSurfaceChanged(GL10 glUnused, int width, int height) {
@@ -22,7 +18,7 @@ public class TEUtilRenderer implements GLSurfaceView.Renderer {
     }
 
     public void onDrawFrame(GL10 glUnused) {
-        GLES20.glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+        GLES20.glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
         GLES20.glClear( GLES20.GL_COLOR_BUFFER_BIT);
     }
 }
